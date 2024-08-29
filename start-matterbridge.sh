a9c6a597b757a00912b5003bbb5faca8cf917749
tionebDotsamapha <tioneb.dotsamapha@gmail.com>
2024-08-29 14:37:30 +0200

    refactor: clean start-matterbridge.sh

--- start-matterbridge.sh
+++ start-matterbridge.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 # Remplacer les tokens dans la configuration
 sed -i "s|{TELEGRAM_BOT_1}|$TELEGRAM_BOT_1|g" /etc/matterbridge/matterbridge.toml
 sed -i "s|{TELEGRAM_BOT_2}|$TELEGRAM_BOT_2|g" /etc/matterbridge/matterbridge.toml
@@ -18,22 +17,4 @@ cat /etc/matterbridge/matterbridge.toml
 echo "Attente de 5 secondes avant de lancer Matterbridge"
 sleep 5
 
-# Vérifier l'emplacement de l'exécutable matterbridge
-which matterbridge
-
 exec matterbridge -conf /etc/matterbridge/matterbridge.toml
-
-## Lancer Matterbridge
-#if [ -x "$(command -v matterbridge)" ]; then
-#  echo "Lancement de Matterbridge"
-#  exec matterbridge
-#else
-#  echo "Erreur : matterbridge n'est pas trouvé dans le PATH"
-#  echo "Contenu du répertoire courant :"
-#  ls -la
-#  echo "Contenu de /bin :"
-#  ls -la /bin
-#  echo "Contenu de /usr/bin :"
-#  ls -la /usr/bin
-#  echo "Contenu de /usr/local/bin :" ls -la /usr/local/bin
-#fi
